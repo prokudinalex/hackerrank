9098806d96503571fe37b31fc648b8b1037f5d41
Alex Prokudin <prokudin89@gmail.com>
2019-01-23 11:14:08 +0700

    player comparator task

---
+++ interview/src/com/prokudin/sorting/PlayerComparator.java
@@ -0,0 +1,48 @@
+package com.prokudin.sorting;
+
+import java.util.*;
+
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player> {
+    // complete this method
+    public int compare(Player a, Player b) {
+        int result = Integer.compare(b.score, a.score);
+        if (0 == result) {
+            // it's stated that player always has a name, so it can't be null
+            // in other case it's better to use StringUtils.compare avoiding NullPointerException
+            result = a.name.compareTo(b.name);
+        }
+        return result;
+    }
+}
+
+
+public class PlayerComparator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
