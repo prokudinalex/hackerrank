14a072a8942cba591740418264d3f10324b717d8
Alex Prokudin <prokudin89@gmail.com>
2019-10-13 20:07:18 +0300

    datatypes task

--- cplusplus/io/io.cpp
+++ cplusplus/io/io.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
-int ioRun() {
+void ioRun() {
     int a;
     int b;
     int c;
     std::cin >> a >> b >> c;
     std::cout << a + b + c << std::endl;
+}
+
+void dataTypesRun() {
+    int i;
+    long l;
+    char ch;
+    float fl;
+    double dbl;
+    scanf("%d %ld %c %f %lf", &i, &l, &ch, &fl, &dbl);
+    printf("%d\n%ld\n%c\n%.3f\n%.9lf\n", i, l, ch, fl, dbl);
 }
